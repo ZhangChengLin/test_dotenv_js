acfff531b5acb9e68d047068e69b3531936c434c
zhangchenglin <469946668@qq.com>
2021-09-22 10:37:21 +0800

    尝试增加替换次数

--- src/test.js
+++ src/test.js
@@ -4,5 +4,18 @@ function test() {
     console.log(www);
     console.log(api);
 }
+function test2() {
+    const www = process.env.DOMAIN_WWW;
+    const api = process.env.DOMAIN_API;
+    console.log(www);
+    console.log(api);
+}
+
+function test3() {
+    const www = process.env.DOMAIN_WWW;
+    const api = process.env.DOMAIN_API;
+    console.log(www);
+    console.log(api);
+}
 
 test()
